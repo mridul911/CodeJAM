b68b39465cea7f67ccc58f9918ebd009302fa736
mridul911 <60569576+mridul911@users.noreply.github.com>
2020-06-03 20:55:49 +0530

    Add files via upload

---
+++ task2/try.py
@@ -0,0 +1,17 @@
+import cv2
+import math
+count = 0
+videoFile = "sentry3.mkv"
+cap = cv2.VideoCapture(videoFile)   # capturing the video from the given path
+frameRate = cap.get(5) #frame rate
+x=1
+while(cap.isOpened()):
+    frameId = cap.get(1) #current frame number
+    ret, frame = cap.read()
+    if (ret != True):
+        break
+    if (frameId % math.floor(frameRate) == 0):
+        filename ="frame%d.jpg" % count;count+=1
+        cv2.imwrite(filename, frame)
+cap.release()
+print ("Done!")
